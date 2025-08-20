a420340955c8f230bdc48f3dfc297fd92a04548a
hippolite-oss <hippoliteagbodamakou@gmail.com>
2025-08-20 02:11:27 +0100

    update dd fichier content2

--- src/components/Content/Content2/Content2.tsx
+++ src/components/Content/Content2/Content2.tsx
@@ -1,13 +1,11 @@
-
 import "./Content2.css";
 
 const Content2 = () => {
   return (
     <div className="content2-container">
+        <h4>Future Payment</h4>
       <div className="top-row">
         <div className="box box1">
-          <h4>Future Payment</h4>
-          <br />
           <h2>Experience That Grows with Your Scale</h2>
         </div>
         <div className="box box2">
@@ -16,13 +14,16 @@ const Content2 = () => {
       </div>
       <div className="bottom-row">
         <div className="box box3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos repellendus amet repellat sunt quae, delectus explicabo cumque voluptates eligendi odio libero quia accusamus facere obcaecati ut nemo, fugit consequuntur recusandae!
+          <h3>Financial Efficiency</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos repellendus amet repellat sunt quae, delectus explicabo cumque voluptates eligendi odio libero quia accusamus fa</p>
         </div>
         <div className="box box4">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium asperiores, officia ut sit maxime vitae fugit similique nam ullam culpa nesciunt sunt totam quasi maiores odit eveniet sint voluptatem autem.
+          <h3>Cost Reduction</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium asperiores, officia ut sit maxime vitae fugit similique nam ullam culpa nesciunt sunt totam quasi maiore</p>
         </div>
         <div className="box box5">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, animi adipisci suscipit aliquid esse ratione, dicta voluptatibus ad voluptatem expedita quam nesciunt omnis, nisi laboriosam consectetur repudiandae alias odit error!
+          <h3>Revenue Growth</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, animi adipisci suscipit aliquid esse ratione, dicta voluptatibus ad voluptatem expedita quam nesciunt</p>
         </div>
       </div>
     </div>
