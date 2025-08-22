c8bb45d8e3c1068b41eac25b4d9232c12028f518
hippolite-oss <hippoliteagbodamakou@gmail.com>
2025-08-23 00:03:06 +0100

    commit d'amelioration du fichier header.tsx et du fichier header.css

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,46 +6,50 @@ import logo from "../../assets/logo-cauris.svg";
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const closeMenu = () => setIsMenuOpen(false);
 
   return (
-    <header className="navbar">
-      {/* Logo + Nom */}
-      <div className="navbar-logo">
-        <img src={logo} alt="Logo CaurisPay" />
-        <span className="navbar-brand">CaurisPay</span>
-      </div>
-
-      {/* Menu Hamburger pour mobile */}
-      <button 
-        className={`hamburger ${isMenuOpen ? 'open' : ''}`} 
-        onClick={toggleMenu}
-        aria-label="Menu"
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-
-      {/* Liens et boutons dans un conteneur pour le menu mobile */}
-      <div className={`navbar-content ${isMenuOpen ? 'open' : ''}`}>
-        {/* Liens */}
-        <nav className="navbar-links">
-          <a href="#products" onClick={() => setIsMenuOpen(false)}>Products</a>
-          <a href="#custumers" onClick={() => setIsMenuOpen(false)}>Customers</a>
-          <a href="#pricing" onClick={() => setIsMenuOpen(false)}>Pricing</a>
-          <a href="#learn" onClick={() => setIsMenuOpen(false)}>Learn</a>
-        </nav>
-
-        {/* Boutons Login / Signup */}
-        <div className="navbar-buttons">
-          <Link to="/connexion" className="btn login" onClick={() => setIsMenuOpen(false)}>Login</Link>
-          <Link to="/inscription" className="btn signup" onClick={() => setIsMenuOpen(false)}>Sign Up</Link>
+    <>
+      <header className="navbar">
+        {/* Logo */}
+        <div className="navbar-logo">
+          <img src={logo} alt="Logo CaurisPay" />
+          <span className="navbar-brand">CaurisPay</span>
         </div>
-      </div>
-    </header>
+
+        {/* Hamburger */}
+        <button
+          className={`hamburger ${isMenuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+          aria-label="Menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        {/* Menu Desktop/Mobile */}
+        <div className={`navbar-content ${isMenuOpen ? "open" : ""}`}>
+          <nav className="navbar-links">
+            <a href="#products" onClick={closeMenu}>Products</a>
+            <a href="#customers" onClick={closeMenu}>Customers</a>
+            <a href="#pricing" onClick={closeMenu}>Pricing</a>
+            <a href="#learn" onClick={closeMenu}>Learn</a>
+          </nav>
+          <div className="navbar-buttons">
+            <Link to="/connexion" className="btn login" onClick={closeMenu}>Login</Link>
+            <Link to="/inscription" className="btn signup" onClick={closeMenu}>Sign Up</Link>
+          </div>
+        </div>
+      </header>
+
+      {/* Overlay */}
+      <div
+        className={`navbar-overlay ${isMenuOpen ? "open" : ""}`}
+        onClick={closeMenu}
+      ></div>
+    </>
   );
 };
 
