a1522ed0ed42f66a63c11b37cf20aab0475deddc
hippolite-oss <hippoliteagbodamakou@gmail.com>
2025-08-22 23:49:18 +0100

    commit pour afficheer le bacground

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Connexion from "./Pages/Connexion";
@@ -7,15 +6,12 @@ import Content1 from "./components/Content1/Content1";
 import Content2 from "./components/Content/Content2/Content2";
 import Content3 from "./components/Content/Content3/Content3";
 import Content4 from "./components/Content/Content4/Content4";
-import './App.css'
-
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
-
       <Routes>
         <Route path="/Connexion" element={<Connexion />} />
         <Route path="/Inscription" element={<Inscription />} />
@@ -23,11 +19,10 @@ function App() {
           path="/"
           element={
             <>
-            
-          <Content1/>
-          <Content2/>
-          <Content3/>
-          <Content4/>   
+              <Content1 />
+              <Content2 />
+              <Content3 />
+              <Content4 />
             </>
           }
         />
