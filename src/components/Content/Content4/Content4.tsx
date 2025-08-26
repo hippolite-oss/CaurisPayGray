18b7f01c46be809e539b2ef57bd5b85ab37d2e8f
hippolite-oss <hippoliteagbodamakou@gmail.com>
2025-08-26 15:32:39 +0100

    rendrele fichier  Content4 responsive pour mobile

--- src/components/Content/Content4/Content4.tsx
+++ src/components/Content/Content4/Content4.tsx
@@ -3,28 +3,28 @@ import './Content4.css'
 const Content4 = () => {
   return (
     <section className="section">
-            <h5>STEP</h5>
-            <p className="p">
-                Maximize your returns with a <br /> Reserve account that generates 
-            </p>
-            <div className="div">
-                <div className="div-1">
-                    <h1>1</h1>
-                    <h2>Open your account </h2>
-                    <p>Sign Up to CaurisPay and set up your count from the dashboard</p>
-                </div>
-                <div className="div-2">
-                    <h1>2</h1>
-                    <h2>Transfers your money </h2>
-                    <p>Move money from to another account into and start to earning up</p>
-                </div>
-                <div className="div-3">
-                    <h1>3</h1>
-                    <h2>Watch your balance grow </h2>
-                    <p>Accessed instantly and remain Lorem ipsum dolor sit amet.sit amet.</p>
-                </div>
-            </div>
-        </section>
+      <h5>STEP</h5>
+      <p className="p">
+        Maximize your returns with a Reserve account that generates 
+      </p>
+      <div className="div">
+        <div className="div-item">
+          <h1>1</h1>
+          <h2>Open your account </h2>
+          <p>Sign Up to CaurisPay and set up your count from the dashboard</p>
+        </div>
+        <div className="div-item">
+          <h1>2</h1>
+          <h2>Transfers your money </h2>
+          <p>Move money from to another account into and start to earning up</p>
+        </div>
+        <div className="div-item">
+          <h1>3</h1>
+          <h2>Watch your balance grow </h2>
+          <p>Accessed instantly and remain Lorem ipsum dolor sit amet.sit amet.</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
